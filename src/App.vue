74c4bd8230461de5d521ab6684b805e3d9d35de9
sofiaedv <sofiaedv@kth.se>
2024-10-14 22:54:19 +0000

    hashroutes!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { routes } from './router';
 </script>
 
 <template>
@@ -39,47 +39,37 @@ nav {
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+import { routes } from './router';
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  name: 'App',
+  data() {
+    return {
+      routes
+    };
+  },
+  methods: {
+    formatRouteName(name) {
+      // Capitalize the first letter for display in the navigation bar
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    }
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<template>
+  <div>
+    <nav>
+      <RouterLink
+        v-for="route in routes"
+        :key="route.path"
+        :to="route.path"
+      >
+        {{ formatRouteName(route.name) }}
+      </RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
